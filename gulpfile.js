7a139d6f13d305dbf7cd480ab355aa90b33d41f7
nicky <nikkalaancing@gmail.com>
2022-09-20 13:51:05 +0700

    Added base project files.

---
+++ gulpfile.js
@@ -0,0 +1,105 @@
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const del = require('del');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify');
+
+const paths = {
+  styles: {
+    src: 'src/styles/**/*.css',
+    dest: 'build/styles/',
+  },
+  html: {
+    src: 'src/*.html',
+    dest: 'build/',
+  },
+  scripts: {
+    src: 'src/scripts/**/*.js',
+    dest: 'build/js/',
+  },
+};
+
+/**
+ * removed build catalog
+ */
+function removeBuild() {
+  console.info('removed build catalog');
+
+  return del(['build/**/*']);
+}
+
+/**
+ * control and replacement of changed html
+ */
+function copyHtml() {
+  console.info('html file replaced');
+
+  return gulp
+    .src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browserSync.stream());
+}
+
+/**
+ * assembly and control of style changes
+ */
+function styles() {
+  console.info('style file has been compiled');
+
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(
+      rename({
+        basename: 'main',
+        suffix: '.min',
+      })
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream());
+}
+
+function scripts() {
+  return gulp
+    .src(paths.scripts.src)
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+/**
+ * tracking changes in project files.
+ */
+function observer() {
+  console.info('observer has been started');
+
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.html.src, copyHtml);
+  gulp.watch(paths.html.dest).on('change', browserSync.reload);
+  gulp.watch(paths.scripts.src, scripts);
+}
+
+/**
+ * start and restart the project server
+ */
+function sync() {
+  console.info('browser sync function has been started');
+
+  browserSync.init({
+    server: {
+      baseDir: 'build/',
+    },
+  });
+}
+
+/**
+ * Assembly of the final project
+ */
+gulp.task(
+  'default',
+  gulp.parallel(removeBuild, copyHtml, styles, scripts, observer, sync)
+);
+gulp.task('build', gulp.series(removeBuild, copyHtml, styles, scripts));
---
+++ src/scripts/main.js
@@ -0,0 +1,68 @@
+const questions = [
+  {
+    question: 'Сколько на данный момент существует языков программирования?',
+    answers: ['50', '1500', '8000', '900'],
+    correct: 3,
+  },
+  {
+    question: 'В каком году появился JavaScript?',
+    answers: ['В 1987', 'В 1995', 'В 2001', 'В 1989'],
+    correct: 2,
+  },
+  {
+    question: 'Какой компанией был создан JavaScript?',
+    answers: ['Sun', 'Microsoft', 'Netscape', 'Oracle'],
+    correct: 3,
+  },
+  {
+    question: 'Каким было первое названия языка JavaScript?',
+    answers: ['Java', 'LiveScript', 'Mocha', 'CoffeScript'],
+    correct: 4,
+  },
+  {
+    question: 'Что такое ECMAScript?',
+    answers: [
+      'Язык программирования',
+      'Язык разметки',
+      'Спецификация',
+      'Язык стилей',
+    ],
+    correct: 3,
+  },
+  {
+    question: 'Кто является создателем языка JavaScript?',
+    answers: [
+      'Тим Бернерс-Ли',
+      'Брендан Эйх',
+      'Джеймс Гослинг',
+      'Тимоти Бернс',
+    ],
+    correct: 2,
+  },
+  {
+    question:
+      'Как называется язык программирования от компании Microsoft, который во многом аналогичен языку JavaScript?',
+    answers: ['JS', 'JScript', 'ActionScript', 'CoffeScript'],
+    correct: 2,
+  },
+  {
+    question:
+      'Может ли JavaScript на данный момент времени быть использован на стороне сервера?',
+    answers: ['Да', 'Нет', 'Нет ответа', 'Что такое сервер'],
+    correct: 1,
+  },
+  {
+    question: 'Как называется первый в истории движок JavaScript?',
+    answers: ['SpiderMonkey', 'Rhino', 'Drosera', 'V1'],
+    correct: 1,
+  },
+  {
+    question:
+      'Какая из популярных JavaScript-библиотека предназначена для разработки пользовательских интерфейсов?',
+    answers: ['Node.js', 'React.js', 'Prototype', 'jQuery'],
+    correct: 2,
+  },
+];
+
+let questionIndex = 0;
+let score = 0;
